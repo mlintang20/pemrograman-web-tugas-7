8bd6d56a1efa3a928d05c12bf57d3f900a3e84e0
mlintangp <lintangpanjerino.2002@gmail.com>
2022-11-15 20:56:14 +0700

    style: dark mode

--- form-daftar.php
+++ form-daftar.php
@@ -9,21 +9,21 @@
   <!-- CDN Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body class="bg-dark text-light">
   <header>
     <h1 class="text-center my-3">Formulir Pendaftaran Siswa Baru</h1>
   </header>
 
   <form action="proses-pendaftaran.php" method="POST" class="d-flex justify-content-center mt-5">
-    <fieldset>
+    <fieldset class="p-4 border border-3 border-secondary rounded">
 
       <div class="my-3">
         <label for="nama">Nama: </label>
-        <input type="text" name="nama" placeholder="nama lengkap" />
+        <input type="text" name="nama" placeholder="Nama Lengkap" class="bg-dark text-light" />
       </div>
       <div class="my-3">
         <label for="alamat">Alamat: </label>
-        <textarea name="alamat"></textarea>
+        <textarea name="alamat" class="bg-dark text-light"></textarea>
       </div>
       <div class="my-3">
         <label for="jenis_kelamin">Jenis Kelamin: </label>
@@ -32,7 +32,7 @@
       </div>
       <div class="my-3">
         <label for="agama">Agama: </label>
-        <select name="agama">
+        <select name="agama" class="bg-dark text-light">
             <option>Islam</option>
             <option>Kristen</option>
             <option>Katolik</option>
@@ -42,10 +42,10 @@
       </div>
       <div class="my-3">
         <label for="sekolah_asal">Sekolah Asal: </label>
-        <input type="text" name="sekolah_asal" placeholder="nama sekolah" />
+        <input type="text" name="sekolah_asal" placeholder="Nama Sekolah" class="bg-dark text-light" />
       </div>
       <div class="d-flex justify-content-center">
-        <input type="submit" value="Daftar" name="daftar" class="mt-3 bg-primary text-light">
+        <input type="submit" value="Daftar" name="daftar" class="mt-3 bg-success text-light">
       </div>
 
     </fieldset>
--- form-edit.php
+++ form-edit.php
@@ -29,22 +29,23 @@
   <!-- CDN Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body class="bg-dark text-light">
   <header>
     <h1 class="text-center my-3">Formulir Edit Siswa</h1>
   </header>
 
   <form action="proses-edit.php" method="POST" class="d-flex justify-content-center mt-5">
-    <fieldset>
+    <fieldset class="p-4 border border-3 border-secondary rounded">
+
       <input type="hidden" name="id" value="<?php echo $siswa['id'] ?>" />
 
       <div class="my-3">
           <label for="nama">Nama: </label>
-          <input type="text" name="nama" placeholder="nama lengkap" value="<?php echo $siswa['nama'] ?>" />
+          <input type="text" name="nama" placeholder="Nama Lengkap" class="bg-dark text-light" value="<?php echo $siswa['nama'] ?>" />
       </div>
       <div class="my-3">
           <label for="alamat">Alamat: </label>
-          <textarea name="alamat"><?php echo $siswa['alamat'] ?></textarea>
+          <textarea name="alamat" class="bg-dark text-light"><?php echo $siswa['alamat'] ?></textarea>
       </div>
       <div class="my-3">
           <label for="jenis_kelamin">Jenis Kelamin: </label>
@@ -55,7 +56,7 @@
       <div class="my-3">
           <label for="agama">Agama: </label>
           <?php $agama = $siswa['agama']; ?>
-          <select name="agama">
+          <select name="agama" class="bg-dark text-light">
               <option <?php echo ($agama == 'Islam') ? "selected": "" ?>>Islam</option>
               <option <?php echo ($agama == 'Kristen') ? "selected": "" ?>>Kristen</option>
               <option <?php echo ($agama == 'Katolik') ? "selected": "" ?>>Katolik</option>
@@ -65,10 +66,10 @@
       </div>
       <div class="my-3">
           <label for="sekolah_asal">Sekolah Asal: </label>
-          <input type="text" name="sekolah_asal" placeholder="nama sekolah" value="<?php echo $siswa['sekolah_asal'] ?>" />
+          <input type="text" name="sekolah_asal" placeholder="Nama Sekolah" class="bg-dark text-light" value="<?php echo $siswa['sekolah_asal'] ?>" />
       </div>
       <div class="d-flex justify-content-center">
-        <input type="submit" value="Simpan" name="simpan" class="mt-3 bg-primary text-light" />
+        <input type="submit" value="Simpan" name="simpan" class="mt-3 bg-warning text-dark" />
       </div>
     </fieldset>
   </form>
